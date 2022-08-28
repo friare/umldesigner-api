d46f308324bf5958dc37b7221ba88570aaa15d03
Généreux akotenou <genereux.akotenou@gmail.com>
2022-08-28 13:14:53 +0100

    Add startup cmd for azure

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ app.include_router(seeder.router)
 
 #main
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port='8000')
+    uvicorn.run("main:app", host='0.0.0.0', port=8000)
 
  

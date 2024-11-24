182f0b9571aa0a8989b02cbf553ace696a58c9bd
friare-gitbot <145939912+friare-gitbot@users.noreply.github.com>
2024-11-24 21:27:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,11 @@ models.Base.metadata.create_all(engine)
 
 #cross origin
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    ""
+    "https://umldesigner.app.friare.org",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
